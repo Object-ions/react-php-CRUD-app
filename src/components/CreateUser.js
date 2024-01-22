be7beac2bf0387cbb5635a0f98176da38ed29140
object-ions <moshikoatia@gmail.com>
2024-01-22 15:18:58 -0800

    add form, collect data from form

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateUser = () => {
+  const [inputs, setInputs] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
   return (
     <div>
       <h1>CreateUser</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input type="text" name="name" onChange={handleChange} />
+        <br />
+        <label>Email:</label>
+        <input type="text" name="email" onChange={handleChange} />
+        <br />
+        <label>Mobile:</label>
+        <input type="text" name="mobile" onChange={handleChange} />
+
+        <br />
+        <button>Save</button>
+      </form>
     </div>
   );
 };

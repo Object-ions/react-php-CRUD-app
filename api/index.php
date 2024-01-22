de703e500054cef59268bbce2f2f93b50c6df973
object-ions <moshikoatia@gmail.com>
2024-01-22 15:52:52 -0800

    add essential files for Axios api POST request

---
+++ api/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+this file should be included inside XAMPP directory:
+'/Applications/XAMPP/xamppfiles/htdocs/api(current folder)
+*/
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+echo "Testing";
